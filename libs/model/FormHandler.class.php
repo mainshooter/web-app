8f3a356299fb0d666689bac48dbc1cb3d3e5e95e
Peter 4A5A <498883@edu.rocmn.nl>
2018-01-23 08:28:24 +0100

    Added documentation

--- libs/model/FormHandler.class.php
+++ libs/model/FormHandler.class.php
@@ -9,10 +9,18 @@
       $this->Security = new Security();
     }
 
+    /**
+     * Sets a post request key who is required for the form
+     * @param [string] $keyname [The name of the post key]
+     */
     public function setRequired($keyname) {
       $this->keys[] = $this->Security->check_input($keyname);
     }
 
+    /**
+     * Runs the formhandler and checks if all keys are set and valid
+     * @return [boolean] [Return truw if everything went well]
+     */
     public function run() {
       if (!empty($this->keys)) {
         foreach ($this->keys as $key) {
@@ -30,6 +38,11 @@
       }
     }
 
+    /**
+     * Gets the value from a post cleaned
+     * @param  [string] $postKey [The name of the post key]
+     * @return [string]          [The result sanitized]
+     */
     public function getPostValue($postKey) {
       if (ISSET($_POST[$postKey]) === true) {
         $result = $this->Security->check_input($_POST[$postKey]);
