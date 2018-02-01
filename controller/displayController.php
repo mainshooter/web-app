11d3c92747389340b0ab30b09ff83e295aa25e6c
Peter 4A5A <498883@edu.rocmn.nl>
2018-02-01 09:38:37 +0100

    Added as a property

--- controller/displayController.php
+++ controller/displayController.php
@@ -2,18 +2,18 @@
   require_once APP_PATH . '/libs/model/Language.class.php';
 
   class displayController {
+    private $Language;
 
     public function __construct() {
-
+      $this->Language = new Language(APP_LANG);
     }
 
     /**
      * The default method of the controller that present the default view
      */
     public function index() {
-      $Language = new Language(APP_LANG);
-      $data['title'] = $Language->get('home');
-      $data['content'] = $Language->get('home_intro_text');
+      $data['title'] = $this->Language->get('home');
+      $data['content'] = $this->Language->get('home_intro_text');
 
       loadHeader();
       loadView('display/home.php', $data);
